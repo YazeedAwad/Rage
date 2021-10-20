c2e5ec614cee90e5ec8aecf4b60739287e3892fe
Yazeed <yazeedawad009@gmail.com>
2021-10-21 00:57:26 +0300

    Rage

---
+++ pages/basket.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div class="topnav">
+        <a class="active a-link" href="/">
+          Home
+        </a>
+</div>
+       </>
+  )}
--- pages/index.js
+++ pages/index.js
@@ -5,43 +5,43 @@ export default function Home() {
   return (
     <>
       <div class="topnav">
-        <a class="active" href="#home">
+        <a class="active a-link" href="/">
           Home
         </a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-        <a href="#cart">Cart</a>
-        <svg
-        class="svgbasket"
-          width="20"
-          height="22"
-          viewBox="0 0 20 22"
-          fill="#c71714"
-          stroke="currentColor"
-        >
-          <path
-            d="M4 1L1 5V19C1 19.5304 1.21071 20.0391 1.58579 20.4142C1.96086 20.7893 2.46957 21 3 21H17C17.5304 21 18.0391 20.7893 18.4142 20.4142C18.7893 20.0391 19 19.5304 19 19V5L16 1H4Z"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          ></path>
-          <path
-            d="M1 5H19"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          ></path>
-          <path
-            d="M14 9C14 10.0609 13.5786 11.0783 12.8284 11.8284C12.0783 12.5786 11.0609 13 10 13C8.93913 13 7.92172 12.5786 7.17157 11.8284C6.42143 11.0783 6 10.0609 6 9"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          ></path>
-        </svg>
+        <a href="/basket">
+          <svg
+            class="svgbasket"
+            width="20"
+            height="22"
+            viewBox="0 0 20 22"
+            fill="#c71714"
+            stroke="currentColor"
+          >
+            <path
+              d="M4 1L1 5V19C1 19.5304 1.21071 20.0391 1.58579 20.4142C1.96086 20.7893 2.46957 21 3 21H17C17.5304 21 18.0391 20.7893 18.4142 20.4142C18.7893 20.0391 19 19.5304 19 19V5L16 1H4Z"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+            <path
+              d="M1 5H19"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+            <path
+              d="M14 9C14 10.0609 13.5786 11.0783 12.8284 11.8284C12.0783 12.5786 11.0609 13 10 13C8.93913 13 7.92172 12.5786 7.17157 11.8284C6.42143 11.0783 6 10.0609 6 9"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+        </a>
       </div>
-
+      <h2>
+        <p class="p1"> All Product</p>
+      </h2>
       <div>
-        <h2 class="h1">Gaming Store</h2>
         <p class="p1">
           <div class="columns">
             <ul class="price">
